0f87190bf19525aa0f49728e91425bb01b128ce8
Khalid <khalidanwarantur@gmail.com>
2025-09-17 01:43:46 +0600

    good night

---
+++ src/data/usersData.js
@@ -0,0 +1,7 @@
+export const users = [
+  { id: 1, name: "Khalid", email: "khalid@example.com", role: "Admin" },
+  { id: 2, name: "Nadim", email: "nadim@example.com", role: "User" },
+  { id: 3, name: "Samir", email: "samir@example.com", role: "User" },
+  { id: 4, name: "Sujana", email: "sujana@example.com", role: "Manager" },
+  { id: 5, name: "Momo", email: "momo@example.com", role: "User" },
+];
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { users } from '../data/usersData'
 
-const users = () => {
+const Users = () => {
+  const [state, setState] = useState(users)
+  const [sortAsc, setSortAsc] = useState(true)
+
+  const sortByName = ()=>{
+    const sorted = [...state].sort((a,b)=>
+     sortAsc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    )
+    setState(sorted)
+    setSortAsc(!sortAsc)
+  }
   return (
-    <div className="text-3xl font-bold">users</div>
+    <div className="overflow-x-auto p-4">
+      <h2 className='font-bold text-2xl mb-4'>Users</h2>
+      <table className='min-w-full bg-white border'>
+        <thead>
+          <tr className='bg-gray-200'>
+            <th className='px-4 py-2 border'>Id</th>
+            <th className='px-4 py-2 border cursor-pointer' onClick={sortByName}>Name {sortAsc? '↑' : '↓'}</th>
+            <th className='px-4 py-2 border'>Email</th>
+            <th className='px-4 py-2 border'>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            state.map((user)=>(
+              <tr key={user.id} className='text-center'>
+                <td className='px-4 py-2 border'>{user.id}</td>
+                <td className='px-4 py-2 border'>{user.name}</td>
+                <td className='px-4 py-2 border'>{user.email}</td>
+                <td className='px-4 py-2 border'>{user.role}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
-export default users
+export default Users
