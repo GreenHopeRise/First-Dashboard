be7a316989f31f78cfa73132c61edf348629ec02
Khalid <khalidanwarantur@gmail.com>
2025-09-18 08:10:47 +0600

    product section done

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,245 @@
-import React from "react";
-
+import React, { use, useState } from "react";
+import { products as Data } from "../data/productsData";
+import { GrDocumentUpdate } from "react-icons/gr";
+import { MdAddCircleOutline, MdAssignmentAdd, MdOutlineDeleteSweep } from "react-icons/md";
 const Products = () => {
-  return <div className="text-3xl font-bold">Products</div>;
+  const [products, setProducts] = useState(Data);
+
+  // Table Sorting function
+  const [sortAsc, setSortAsc] = useState(true);
+  const sortByName = () => {
+    const sorted = [...products].sort((a, b) =>
+      sortAsc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    );
+    setProducts(sorted);
+    setSortAsc(!sortAsc);
+  };
+
+  // Search or Filtering
+  const [query, setQuery] = useState("");
+  const filterProduct = products.filter(
+    (product) =>
+      product.name.toLocaleLowerCase().includes(query.toLocaleLowerCase()) ||
+      product.category.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  );
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const productPerPage = 10;
+  const lastProductIndex = currentPage * productPerPage;
+  const firstProductIndex = lastProductIndex - productPerPage;
+  const currentProductPage = filterProduct.slice(
+    firstProductIndex,
+    lastProductIndex
+  );
+
+  // Data Add Update
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(null);
+  const [category, setCategory] = useState("");
+  const [stock, setStock] = useState(null);
+  const [updateProductId, setUpdateProductId] = useState(null);
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (!name || !price || !category || !stock) {
+      alert("Please fill all fields");
+      return;
+    }
+    if (updateProductId !== null) {
+      const updateProduct = products.map((product) =>
+        product.id === updateProductId
+          ? { ...product, name, price, category, stock }
+          : product
+      );
+      setProducts(updateProduct);
+      setUpdateProductId(null);
+      setName("");
+      setPrice("");
+      setCategory("");
+      setStock("");
+      return;
+    }
+    const newProduct = {
+      id: products.length + 1,
+      name,
+      price,
+      category,
+      stock,
+    };
+    setProducts([newProduct, ...products]);
+    setName("");
+    setPrice("");
+    setCategory("");
+    setStock("");
+  };
+
+  // Delete 
+  const handleDelete = (id)=>{
+    const newProducts = products.filter((product)=>product.id !== id)
+    setProducts(newProducts)
+  }
+
+
+  // xml here
+  return (
+    <div className="overflow-x-auto bg-gray-50 rounded-lg shadow-md p-6">
+      <h2 className="font-bold text-3xl mb-6 text-gray-800 ">Products</h2>
+      <input
+        type="text"
+        placeholder="Enter name or catagory"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        className="border border-gray-300 focus:right-2 focus:ring-blue-400 focus:border-blue-400 px-4 py-3 mb-6 rounded-md transition duration-200"
+      />
+      <table className="min-w-full bg-white border border-gray-200 rounded shadow-xl">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="px-6 py-3  border-b border-gray-300 text-left text-sm font-semibold text-gray-700  ">
+              Index
+            </th>
+            <th
+              className="px-6 py-3  border-b border-gray-300 text-left text-sm font-semibold text-gray-700 cursor-pointer hover:text-blue-600"
+              onClick={sortByName}
+            >
+              Name {sortAsc ? "↑" : "↓"}
+            </th>
+            <th className="px-6 py-3  border-b border-gray-300 text-left text-sm font-semibold text-gray-700 ">
+              Price
+            </th>
+            <th className="px-6 py-3  border-b border-gray-300 text-left text-sm font-semibold text-gray-700 ">
+              Category
+            </th>
+            <th className="px-6 py-3  border-b border-gray-300 text-left text-sm font-semibold text-gray-700 ">
+              Stock
+            </th>
+            <th className="px-6 py-3  border-b border-gray-300 text-left text-sm font-semibold text-gray-700 ">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentProductPage.map((product) => (
+            <tr
+              key={product.id}
+              className="text-gray-700 hover:bg-blue-50 transition-colors"
+            >
+              <td className="px-6 py-4 border-b border-gray-200">
+                {product.id}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                {product.name}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                ${product.price}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                {product.category}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                {product.stock}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                <div className="flex gap-3">
+                  {/* Update Button */}
+                  <button
+                    className="p-2 rounded-full bg-blue-100 cursor-pointer text-blue-600 hover:bg-blue-200 transition"
+                    title="Update"
+                    onClick={() => {
+                      setUpdateProductId(product.id);
+                      setName(product.name);
+                      setPrice(product.price);
+                      setCategory(product.category);
+                      setStock(product.stock);
+                    }}
+                  >
+                    <GrDocumentUpdate size={18} />
+                  </button>
+
+                  {/* Delete Button */}
+                  <button
+                    className="p-2 cursor-pointer rounded-full bg-red-100 text-red-600 hover:bg-red-200 transition"
+                    title="Delete"
+                    onClick={()=>handleDelete(product.id)}
+                  >
+                    <MdOutlineDeleteSweep size={18} />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex gap-3 mt-6 justify-center items-center">
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`px-4 py-2 rounded-md font-medium transition-colors ${
+            currentPage === 1
+              ? "bg-gray-300 cursor-not-allowed text-gray-600"
+              : "bg-blue-600 hover:bg-blue-700 text-white"
+          }`}
+        >
+          Previous
+        </button>
+        <br />
+        <button
+          className={`px-4 py-2 rounded-md font-medium transition-colors ${
+            currentPage === Math.ceil(filterProduct.length / productPerPage)
+              ? "bg-gray-300 cursor-not-allowed text-gray-600"
+              : "bg-blue-600 hover:bg-blue-700 text-white"
+          }`}
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={
+            currentPage === Math.ceil(filterProduct.length / productPerPage)
+          }
+        >
+          Next
+        </button>
+        <span className="text-sm font-medium text-gray-700 bg-gray-100 px-3 py-1 rounded">
+          {currentPage}/{Math.ceil(filterProduct.length / productPerPage)}
+        </span>
+      </div>
+      <div className="mt-8">
+        <form
+          className="flex flex-col sm:flex-row gap-4 items-center justify-center"
+          onSubmit={handleAdd}
+        >
+          <input
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
+          />
+          <input
+            type="number"
+            placeholder="Enter price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
+          />
+          <input
+            type="text"
+            placeholder="Enter category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
+          />
+          <input
+            type="number"
+            placeholder="Enter stock"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+            className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
+          />
+          <button type="submit"
+          className="border-none  rounded-full p-3 bg-green-200 hover:bg-green-300 ">
+            <MdAddCircleOutline className="size-6 text-green-800"/>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,131 +1,206 @@
-import React, { useState } from 'react'
-import { users } from '../data/usersData'
+import React, { useState } from "react";
+import { users } from "../data/usersData";
 
 const Users = () => {
-  const [state, setState] = useState(users)
-  const [sortAsc, setSortAsc] = useState(true)
-  const [query, setQuery] = useState('')
-  const [currentPage, setCurrentPage] = useState(1)
-  const userPerPage = 10
-  const lastUserIndex = currentPage * userPerPage
-  const firstUserIndex = lastUserIndex - userPerPage
+  const [state, setState] = useState(users);
+  const [sortAsc, setSortAsc] = useState(true);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const userPerPage = 10;
+  const lastUserIndex = currentPage * userPerPage;
+  const firstUserIndex = lastUserIndex - userPerPage;
+
+
+
+// table sorting 
+  const sortByName = () => {
+    const sorted = [...state].sort((a, b) =>
+      sortAsc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    );
+    setState(sorted);
+    setSortAsc(!sortAsc);
+  };
+
+
+
+
+  // Search or filtering 
+  const filterUser = state.filter(
+    (user) =>
+      user.name.toLowerCase().includes(query.toLowerCase()) ||
+      user.email.toLowerCase().includes(query.toLowerCase())
+  );
+
+
   
+  const currentUsers = filterUser.slice(firstUserIndex, lastUserIndex);
 
-  const sortByName = ()=>{
-    const sorted = [...state].sort((a,b)=>
-     sortAsc ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
-    )
-    setState(sorted)
-    setSortAsc(!sortAsc)
-  }
-  const filterUser = state.filter((user)=>
-    user.name.toLowerCase().includes(query.toLowerCase())|| user.email.toLowerCase().includes(query.toLowerCase())
-  )
-  const currentUsers = filterUser.slice(firstUserIndex,lastUserIndex)
-
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [role, setRole] = useState('')
-  const handleAdd = (e)=>{
-    e.preventDefault()
-    if(!name||!email||!role){
-      alert('Please fill all fields')
-      return
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [updateUserId, setUpdateUserId] = useState(null);
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (!name || !email || !role) {
+      alert("Please fill all fields");
+      return;
+    }
+    if(updateUserId !== null){
+      const updateUser = state.map((user)=>
+        user.id === updateUserId ? { ...user,name,email,role } : user
+        
+      )
+      setState(updateUser )
+        setUpdateUserId(null)
+        return
     }
     const newUser = {
       id: state.length + 1,
       name,
       email,
-      role
-    }
-    setState([newUser,...state])
-    setName('')
-    setEmail('')
-    setRole('')
-
+      role,
+    };
+    setState([newUser, ...state]);
+    setName("");
+    setEmail("");
+    setRole("");
+  };
+  const handleDelete = (id) =>{
+    const updateUser = state.filter((user)=>user.id !== id) 
+    setState(updateUser)
   }
+
+  // xml star here
+
   return (
     <div className="overflow-x-auto p-6 bg-gray-50 rounded-lg shadow-md">
-      <h2 className='font-bold text-3xl mb-6 text-gray-800'>Users</h2>
-      <input 
-      type="text" 
-      value={query}
-      placeholder='Search by name or email'
-      onChange={(e)=>setQuery(e.target.value)}
-      className="border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:border-blue-400 px-4 py-3 mb-6 rounded-md transition duration-200"
+      <h2 className="font-bold text-3xl mb-6 text-gray-800">Users</h2>
+      <input
+        type="text"
+        value={query}
+        placeholder="Search by name or email"
+        onChange={(e) => setQuery(e.target.value)}
+        className="border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:border-blue-400 px-4 py-3 mb-6 rounded-md transition duration-200"
       />
-      <table className='min-w-full bg-white border border-gray-200 rounded-md shadow-sm'>
+      <table className="min-w-full bg-white border border-gray-200 rounded-md shadow-sm">
         <thead>
-          <tr className='bg-gray-100'>
-            <th className='px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700'>Id</th>
-            <th 
-              className='px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700 cursor-pointer select-none hover:text-blue-600 flex items-center gap-1' 
-              onClick={sortByName}
-              >
-                Name {sortAsc? '↑' : '↓'}
+          <tr className="bg-gray-100">
+            <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">
+              Id
             </th>
-            <th className='px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700'>Email</th>
-            <th className='px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700'>Role</th>
+            <th
+              className="px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700 cursor-pointer select-none hover:text-blue-600 flex items-center gap-1"
+              onClick={sortByName}
+            >
+              Name {sortAsc ? "↑" : "↓"}
+            </th>
+            <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">
+              Email
+            </th>
+            <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">
+              Role
+            </th>
+            <th className="px-6 py-3 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">
+              Action
+            </th>
           </tr>
         </thead>
         <tbody>
-          {
-            currentUsers.map((user)=>(
-              <tr key={user.id} className='text-gray-700 hover:bg-blue-50 transition-colors'>
-                <td className='px-6 py-4 border-b border-gray-200'>{user.id}</td>
-                <td className='px-6 py-4 border-b border-gray-200'>{user.name}</td>
-                <td className='px-6 py-4 border-b border-gray-200'>{user.email}</td>
-                <td className='px-6 py-4 border-b border-gray-200'>{user.role}</td>
-              </tr>
-            ))
-          }
+          {currentUsers.map((user) => (
+            <tr
+              key={user.id}
+              className="text-gray-700 hover:bg-blue-50 transition-colors"
+            >
+              <td className="px-6 py-4 border-b border-gray-200">{user.id}</td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                {user.name}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                {user.email}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                {user.role}
+              </td>
+              <td className="px-6 py-4 border-b border-gray-200">
+                <div className="flex gap-2">
+                  <button
+                    onClick={() => {
+                      setUpdateUserId(user.id);
+                      setName(user.name);
+                      setEmail(user.email);
+                      setRole(user.role);
+                    }}
+                    className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors font-medium shadow-sm"
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => handleDelete(user.id)}
+                    className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors font-medium shadow-sm"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <div className="flex gap-3 mt-6 justify-center">
-        <button 
-          onClick={()=>setCurrentPage(currentPage-1)} 
-          disabled={currentPage===1}
+      <div className="flex gap-3 mt-6 justify-center items-center">
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
           className={`px-4 py-2 rounded-md font-medium transition-colors ${
-            currentPage===1 ? 'bg-gray-300 cursor-not-allowed text-gray-600' : 'bg-blue-600 hover:bg-blue-700 text-white'
+            currentPage === 1
+              ? "bg-gray-300 cursor-not-allowed text-gray-600"
+              : "bg-blue-600 hover:bg-blue-700 text-white"
           }`}
         >
           Previous
         </button>
-        <button 
-          onClick={()=>setCurrentPage(currentPage+1)} 
-          disabled={currentPage=== Math.ceil(filterUser.length/userPerPage)}
+        
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === Math.ceil(filterUser.length / userPerPage)}
           className={`px-4 py-2 rounded-md font-medium transition-colors ${
-            currentPage=== Math.ceil(filterUser.length/userPerPage) ? 'bg-gray-300 cursor-not-allowed text-gray-600' : 'bg-blue-600 hover:bg-blue-700 text-white'
+            currentPage === Math.ceil(filterUser.length / userPerPage)
+              ? "bg-gray-300 cursor-not-allowed text-gray-600"
+              : "bg-blue-600 hover:bg-blue-700 text-white"
           }`}
         >
           Next
-        </button>
+        </button><span className="px-3 py-1 text-gray-700 font-semibold text-sm">
+          {currentPage} / {Math.ceil(filterUser.length / userPerPage)}
+        </span>
       </div>
       <div className="mt-8">
-        <form onSubmit={handleAdd} className="flex flex-col sm:flex-row gap-4 items-center justify-center">
-          <input 
-            type="text" 
-            placeholder='Enter Name' 
-            value={name} 
-            onChange={(e)=>setName(e.target.value)} 
+        <form
+          onSubmit={handleAdd}
+          className="flex flex-col sm:flex-row gap-4 items-center justify-center"
+        >
+          <input
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
           />
-          <input 
-            type="email" 
-            placeholder='Enter Email' 
-            value={email} 
-            onChange={(e)=>setEmail(e.target.value)} 
+          <input
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
           />
-          <input 
-            type="text" 
-            placeholder='Enter Role' 
-            value={role} 
-            onChange={(e)=>setRole(e.target.value)} 
+          <input
+            type="text"
+            placeholder="Enter Role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-400 w-full sm:w-1/4"
           />
-          <button 
-            type='submit' 
+          <button
+            type="submit"
             className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md font-semibold transition-colors w-full sm:w-auto"
           >
             Add
@@ -133,7 +208,7 @@ const Users = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
